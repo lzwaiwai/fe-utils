f295b308bb7d811ae799f51250b4859d016565ed
lzwaiwai <lzwai3230@foxmail.com>
2017-05-22 11:07:14 +0800

    修改日期函数集写法

--- fe-utils.js
+++ fe-utils.js
@@ -4,29 +4,31 @@
 	(global.feUtils = factory());
 }(this, (function () { 'use strict';
 
-var dateFormat = function dateFormat(d, fmt) {
-  // format(new Date(), 'MM月dd日 hh:mm')
-  var o = {
-    'M+': d.getMonth() + 1, // 月份
-    'd+': d.getDate(), // 日
-    'h+': d.getHours(), // 小时
-    'm+': d.getMinutes(), // 分
-    's+': d.getSeconds(), // 秒
-    'q+': Math.floor((d.getMonth() + 3) / 3), // 季度
-    'S': d.getMilliseconds() // 毫秒
-  };
+var date = {
+  dateFormat: function dateFormat(d, fmt) {
+    // format(new Date(), 'MM月dd日 hh:mm')
+    var o = {
+      'M+': d.getMonth() + 1, // 月份
+      'd+': d.getDate(), // 日
+      'h+': d.getHours(), // 小时
+      'm+': d.getMinutes(), // 分
+      's+': d.getSeconds(), // 秒
+      'q+': Math.floor((d.getMonth() + 3) / 3), // 季度
+      'S': d.getMilliseconds() // 毫秒
+    };
 
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length));
-  }
-
-  for (var k in o) {
-    if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+    if (/(y+)/.test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
-  }
 
-  return fmt;
+    for (var k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+      }
+    }
+
+    return fmt;
+  }
 };
 
 var doc = document;
@@ -275,7 +277,7 @@ var preventViewScroll = function preventViewScroll(classes) {
 };
 
 var main = {
-  date: dateFormat,
+  date: date,
   cookie: cookie,
   toRoman: toRoman,
   urlQuery: urlQuery,
--- libs/date.js
+++ libs/date.js
@@ -1,25 +1,60 @@
-const dateFormat = (d, fmt) => { // format(new Date(), 'MM月dd日 hh:mm')
-  const o = {
-    'M+': d.getMonth() + 1, // 月份
-    'd+': d.getDate(), // 日
-    'h+': d.getHours(), // 小时
-    'm+': d.getMinutes(), // 分
-    's+': d.getSeconds(), // 秒
-    'q+': Math.floor((d.getMonth() + 3) / 3), // 季度
-    'S': d.getMilliseconds() // 毫秒
-  }
-
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length))
-  }
-
-  for (var k in o) {
-    if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+const date = {
+  dateFormat (d, fmt) { // format(new Date(), 'YYYY年MM月DD日 hh:mm:ss')
+    if (!d || !fmt) {
+      throw new Error('lack date or format')
     }
-  }
+    if (typeof(d) === 'string') {
+      d = new Date(d)
+    }
+    const o = {
+      'M+': d.getMonth() + 1, // 月份
+      'D+': d.getDate(), // 日
+      'h+': d.getHours(), // 小时
+      'm+': d.getMinutes(), // 分
+      's+': d.getSeconds(), // 秒
+      'Q+': Math.floor((d.getMonth() + 3) / 3), // 季度
+      'S': d.getMilliseconds() // 毫秒
+    }
 
-  return fmt
+    if (/(Y+)/.test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (d.getFullYear() + '').substr(4 - RegExp.$1.length))
+    }
+
+    for (var k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+      }
+    }
+
+    return fmt
+  }
+  // TodayFormat (date) {
+  //   if (!date) {
+  //     throw new Error('lack date')
+  //   }
+
+  //   const fmt = 'YYYY-MM-DD'
+  //   const cur = new Date() // 当前年月日
+  //   const curYear = cur.getFullYear()
+  //   const curMonth = cur.getMonth() + 1
+  //   const curDay = cur.getDate()
+
+  //   const next = new Date(`${curYear}/${curMonth}/${curDay + 1}`) // 明天年月日
+  //   const param = new Date(date) // 传进来的年月日
+  //   const paramDay = new Date().getDate()
+
+  //   const nowDate = this.dateFormat(cur, fmt)
+  //   const nextDate = this.dateFormat(next, fmt)
+  //   const paramDate = this.dateFormat(param, fmt)
+
+  //   if (nowDate == nextDate) {
+  //     return '今天' + this.dateFormat(paramDate, 'hh:mm')
+  //   } else if (paramDay === curDay) {
+  //     return '明天' + this.dateFormat(paramDate, 'hh:mm')
+  //   } else {
+  //     return this.dateFormat(paramDate, 'MM月DD日 hh:mm')
+  //   }
+  // }
 }
 
-export default dateFormat
+export default date
